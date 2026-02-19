acae6bbf15f3f75c7424bf8e6bb20a9a62f2860c
naim187naim <naimalime.pro@gmail.com>
2026-02-20 00:39:57 +0100

    Correction finale : Ajout de ini_set et permissions mail

--- save.php
+++ save.php
@@ -1,17 +1,18 @@
 <?php
+ini_set('sendmail_path', '/usr/sbin/ssmtp -t');
+
 $conn = new mysqli("localhost", "root", "", "demande_noelie");
 $to = "naimalime.pro@gmail.com";
-$headers = "From: site-noelie@debian.local\r\nContent-Type: text/plain; charset=UTF-8";
 
 if (isset($_POST['choix'])) {
-    $c = $conn->real_escape_string($_POST['choix']);
-    $conn->query("INSERT INTO reponses (choix) VALUES ('CHOIX : $c')");
-    mail($to, "Reponse Noelie : $c", "Elle a clique sur : $c", $headers);
+    $choix = $conn->real_escape_string($_POST['choix']);
+    $conn->query("INSERT INTO reponses (choix) VALUES ('CHOIX : $choix')");
+    mail($to, "Choix de Noelie", "Elle a choisi : " . $choix);
 }
 
 if (isset($_POST['message_texte'])) {
-    $t = $conn->real_escape_string($_POST['message_texte']);
-    $conn->query("INSERT INTO reponses (choix) VALUES ('MESSAGE : $t')");
-    mail($to, "Nouveau message de Noelie", "Message recu :\n\n$t", $headers);
+    $texte = $conn->real_escape_string($_POST['message_texte']);
+    $conn->query("INSERT INTO reponses (choix) VALUES ('MESSAGE : $texte')");
+    mail($to, "Message de Noelie", "Elle t'a ecrit : \n\n" . $_POST['message_texte']);
 }
 ?>
