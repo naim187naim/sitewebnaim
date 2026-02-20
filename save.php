61d02dbb88de88a188096433f6d90c5ec0ca4c95
naim187naim <naimalime.pro@gmail.com>
2026-02-20 01:28:14 +0100

    Retour version stable originale

--- save.php
+++ save.php
@@ -1,15 +1,9 @@
 <?php
+// Remplace par tes vrais identifiants si tu as créé un utilisateur
 $conn = new mysqli("localhost", "root", "", "demande_noelie");
-$to = "naimalime.pro@gmail.com";
 
 if (isset($_POST['choix'])) {
     $choix = $conn->real_escape_string($_POST['choix']);
-    
-    // 1. Sauvegarde BDD
     $conn->query("INSERT INTO reponses (choix) VALUES ('$choix')");
-    
-    // 2. Envoi Mail (Méthode terminal)
-    $sujet = "Choix de Noelie : " . $choix;
-    shell_exec("echo 'Subject: $sujet\n\nNoelie a choisi : $choix' | ssmtp $to");
 }
 ?>
